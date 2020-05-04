58ab00e3046e497c8bc77f09e91001dd47b7bfc2
KarryZ <katejidkova@gmail.com>
2020-05-04 22:28:16 +0300

    states; props; classes

---
+++ src/components/todo-list-item/todo-list-item.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import './todo-list-item.css';
+
+export default class ToDoListItem extends Component {
+  constructor() {
+    super();
+    this.state = {
+      done: false,
+      important: false
+    }
+  }
+
+  onLabelClick = () => {
+    this.setState(({ done }) => {
+      return {
+        done: !done
+      };
+    })
+    console.log(`Done ${this.props.label}`);
+  }
+
+  onMarkLabelImportant = () => {
+    this.setState(({ important }) => {
+      return {
+        important: !important
+      };
+    })
+  }
+
+  render() {
+    const { label } = this.props;
+    const { done, important } = this.state;
+
+    let ClassNames = "todo-list-item";
+    if (done) {
+      ClassNames += " done"
+    }
+    if (important) {
+      ClassNames += " important"
+    }
+
+    return (
+      <span className={ClassNames}>
+        <span
+          className="todo-list-item-label"
+          onClick={this.onLabelClick}>
+          {label}
+        </span>
+
+        <button type="button"
+          className="btn btn-outline-success btn-sm float-right" onClick={this.onMarkLabelImportant}>
+          <i className="fa fa-exclamation" />
+        </button>
+
+        <button type="button"
+          className="btn btn-outline-danger btn-sm float-right">
+          <i className="fa fa-trash-o" />
+        </button>
+      </span>
+    );
+  }
+};
+
+
